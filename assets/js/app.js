59cd3efbc8d75ded8430924ea52c5d6b4e240783
amiribnjaman <amiribnjaman@gmail.com>
2022-02-26 11:32:05 +0600

    Accuweather api's get city and get weather functionalities done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
-console.log('ok')
+//  API's functionalities
+const key = '5ZuMwByk1yKDipuUOHWyHmGfWKULPjtW';
+
+// Get city
+const getCity = async (city) => {
+    const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const res = await fetch(baseUrl + query);
+    const data = await res.json();
+    return data[0];
+
+}
+// Get weather 
+const getWeather = async (cityId) => {
+    const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${cityId}?apikey=${key}`;
+
+    const res = await fetch(baseUrl + query);
+    const data = await res.json();
+    return data[0]
+}
+
